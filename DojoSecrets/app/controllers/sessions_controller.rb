f253a5e85f290deb6b2921c34cc1f911c5a915b1
FLAVIO MOURA ROCHA <fmrocha@gmail.com>
2017-10-16 17:28:08 -0700

    DojoSecrets Authentication

---
+++ DojoSecrets/app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+    def new
+        # render login page
+    end
+
+  def create
+    @user = User.find_by_email(params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to "/users/#{@user.id}"
+    else 
+      flash[:errors] = ["Invalid Authentication"]
+   #  redirect_to "/sessions/new"
+      redirect_to new_session_path
+    end
+  end
+
+    def destroy
+        # Log User out
+        session[:user_id] = nil
+        redirect_to new_session_path
+    end
+
+end
